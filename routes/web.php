d9a1c2d058fb7fb8f29084f173170ab9212a09da
Tommy Kronstål <tommy.kronstal@gmail.com>
2017-09-28 09:42:34 +0200

    First commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+    //return 'happ freomlapp';
+});
+
+Route::get('/hello', function () {
+    return 'happ from happ';
+});
